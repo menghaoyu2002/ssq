d043f1230a43496ac0673863f8bcbbc85187619c
Menghao Yu <menghaoyu2002@gmail.com>
2023-10-10 23:34:07 -0400

    initial cli setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::Parser;
+
+/// Run SQL queries on spreadsheets
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// path of the spreadsheet
+    #[arg(short, long)]
+    path: Option<String>,
+
+    /// name of the sheet within the workbook (for xlsx files only)
+    #[arg(short, long)]
+    sheet: Option<String>,
+
+    /// the query to execute
+    #[arg(short, long)]
+    query: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
 }
