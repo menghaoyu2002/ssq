69f108b30a384180a6937a7166dd67b599fa1997
Menghao Yu <menghaoyu2002@gmail.com>
2023-10-11 22:56:47 -0400

    intial args validation

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,50 @@
-use clap::Parser;
+use std::process::exit;
 
-/// Run SQL queries on spreadsheets
+use clap::Parser;
+use colored::Colorize;
+
+/// Run SQL queries on spreadsheets and outputs the result in JSON
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// path of the spreadsheet
+    /// Path to the spreadsheet
     #[arg(short, long)]
-    path: Option<String>,
+    path: String,
 
-    /// name of the sheet within the workbook (for xlsx files only)
+    /// Name of the sheet within the workbook
     #[arg(short, long)]
     sheet: Option<String>,
 
-    /// the query to execute
+    /// SQL query to execute
     #[arg(short, long)]
     query: String,
 }
 
+const SHEET_REQUIRED: [&str; 7] = ["xlsx", "xls", "xlsm", "xlsb", "xla", "xlam", "ods"];
+const SUPPORTED_FILETYPES: [&str; 8] = ["csv", "xlsx", "xls", "xlsm", "xlsb", "xla", "xlam", "ods"];
+
 fn main() {
     let args = Args::parse();
+
+    let path_parts: Vec<&str> = args.path.split(".").collect();
+    let filetype = path_parts.last().unwrap_or(&"");
+
+    if !SUPPORTED_FILETYPES.contains(filetype) {
+        println!(
+            "{} only {} files are supported",
+            "error:".red().bold(),
+            SUPPORTED_FILETYPES.join(", ").bold().yellow()
+        );
+        exit(1);
+    }
+
+    if SHEET_REQUIRED.contains(filetype) && args.sheet.is_none() {
+        println!(
+            "{} a sheet name {} is required for {} files",
+            "error:".red().bold(),
+            "'--sheet <SHEET>'".yellow(),
+            filetype
+        );
+        exit(1);
+    }
 }
